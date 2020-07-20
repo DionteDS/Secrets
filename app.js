82fa2b01d5e1e5215fe0503dd03f506e27ceebc7
Dionte <Dionte_sb@yahoo.com>
2020-07-19 18:27:57 -0700

    Added a hash for the passwords

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -25,7 +26,7 @@ const userSchema = new mongoose.Schema({
 
 // encrypt the password field from the userSchema
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -46,7 +47,7 @@ app.post("/register", function(req, res) {
 
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     // when we save mongoose will enceypt our password field
@@ -64,7 +65,7 @@ app.post("/register", function(req, res) {
 app.post("/login", function(req, res) {
 
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     // Search for a user in the database that matches the username const
     // Also mongoose will decrypt the password field when we try to find a user
